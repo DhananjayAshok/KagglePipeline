38bcb47bd198dd81bdbed2c9dd6447cd9cb4c6d4
Dhananjay Ashok <dhananjay.ashok99@gmail.com>
2019-12-06 19:31:44 -0500

    Added model parent, added functionality to simple model

--- KagglePipeline/KagglePipeline.py
+++ KagglePipeline/KagglePipeline.py
@@ -9,4 +9,6 @@ if __name__ == "__main__":
     x_test = test[0]
     y_test = test[1]
     model = SimpleHiddenMLP(x_train, y_train, classification=False)
+    model.fit(x_train, y_train, epochs=1000,x_val = x_test, y_val=y_test)
+    model.compare_pred_to_truth(x_test[0:5], y_test[0:5])
 
--- KagglePipeline/models/neural_networks/mlp/SimpleHiddenMLP.py
+++ KagglePipeline/models/neural_networks/mlp/SimpleHiddenMLP.py
@@ -1,10 +1,13 @@
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Dense, Input
 from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.optimizers.schedules import ExponentialDecay
 from tensorflow.keras.losses import CategoricalCrossentropy, MeanSquaredError
 import matplotlib.pyplot as plt
+from models.ModelBase import ModelBase
 
-class SimpleHiddenMLP(object):
+class SimpleHiddenMLP(ModelBase):
     """A Densely Connected Feed Forward MLP which can be used for most classification and regression tasks
 
     Requirements:
@@ -15,6 +18,9 @@ class SimpleHiddenMLP(object):
     -------------
     model : tf.model
         Stores the model reference
+    classification: bool
+        True iff the network solves a classification problem
+        If false then it must be regression
 
     Methods
     -------------
@@ -25,71 +31,95 @@ class SimpleHiddenMLP(object):
     """
    
 
-    def __init__(self, X_train, y_train, X_val = None, y_val = None, n_hidden = 32, classification=True,optimizer=None, loss=None, epochs=100):
+    def __init__(self, X_train, y_train, classification, n_hidden = 32 ,optimizer=None, loss=None):
         """
         Returns a fit model with Input -> 32 Hidden Neurons -> Output for classification. Unless parameters are changed.
+        Default Learning Rate Scheduling is an Adam optimizer with Exponential Decay off an initial learning rate of 0.001
         """
+        self.classification = classification
         self._create_model(X_train, y_train, n_hidden, classification, optimizer, loss)
-        self._fit_model(X_train, y_train, epochs)
         return
 
-    def _create_model(self, X_train, y_train, n_hidden, classification, optimizer=None, loss=None):
+    def _create_model(self, X_train, y_train, n_hidden, classification, optimizer, loss):
         
         i = Input(shape=X_train.shape[1:])
         x = Dense(X_train.shape[1], activation ="elu") (i)
-        x = Dense(n_hidden, activation= "relu") (x)
+        x = Dense(n_hidden, activation= "relu") (x) 
         
-        
-        loss = None
+        l = None
+        metrics = []
         if(classification):
             n_outputs = self._get_num_categories(y_train)
             x = Dense(n_outputs, activation = "softmax") (x)
 
             l = CategoricalCrossentropy()
-
+            metrics.append('accuracy')
 
         else:
             n_outputs = 1
             x = Dense(n_outputs, activation = "relu") (x)
             l = MeanSquaredError()
 
-        opt = Adam(0.001)
+        initial_learning_rate = 0.001
+        lr_schedule = ExponentialDecay(initial_learning_rate,decay_steps=100000,decay_rate=0.96, staircase=True)
+        opt = Adam(learning_rate = lr_schedule)
 
-        if(loss):
+        if(loss is not None):
             l = loss
-        if (optimizer):
+        if (optimizer is not None):
             opt = optimizer
 
         self.model = Model(i, x)
 
         self.model.compile(optimizer=opt,
               loss=l,
-              metrics=['accuracy'])
-        return
-
-
-
-
-           
-
-        
-
-        
-
-
+              metrics=metrics)
         return 
 
-    def _fit_model(self, X_train, y_train, epochs):
-        r = self.model.fit(X_train, y_train, epochs=epochs)
-        #plt.plot(r.history['loss'], label='loss')
-        #plt.show()
+    def fit(self, X_train, y_train, x_val=None, y_val=None, epochs=200, early_stopping_patience=10):
+        """
+        Fits data to the model
+        Defaults:
+            Epochs:200
+            Early Stopping Patience: 10
+        """
+        callbacks = []
+        es = EarlyStopping(monitor='loss', patience=early_stopping_patience)
+        validation_data=None
+        if (x_val is not None):
+            validation_data = (x_val, y_val)
+            es = EarlyStopping(monitor='val_loss', patience=early_stopping_patience)
+        callbacks.append(es)
+        r = self.model.fit(X_train, y_train, epochs=epochs,callbacks=callbacks, validation_data=validation_data)
+
+
+        plt.plot(r.history['loss'], label='loss')
+        if(x_val is not None):
+            plt.plot(r.history['val_loss'], label='val_loss')
+
+        plt.title('Model loss')
+        plt.ylabel('Loss')
+        plt.xlabel('Epoch')
+        plt.legend(['Train', 'Test'], loc='upper left')
+        plt.show()
+
+        if (self.classification):
+            plt.plot(r.history['acc'], label='acc')
+            if(x_val is not None):
+                plt.plot(r.history['val_acc'], label='val_acc')
+            plt.title('Model accuracy')
+            plt.ylabel('Accuracy')
+            plt.xlabel('Epoch')
+            plt.legend(['Train', 'Test'], loc='upper left')
+            plt.show()
         return
 
     def _get_num_categories(self, y):
         return len(set(y))
 
-    def predict(self, X, y):
-        return self.model.predict(X, y)
+    def predict(self, X):
+        return self.model.predict(X)
+
 
 
 
